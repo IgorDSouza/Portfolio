ce61d20194f9d79052b567ff6eabb5cf321151c2
IgorD.Souza <igor251999@hotmail.com>
2023-01-16 21:49:39 -0300

    mudanças pedidas por usuarios

--- js/scroll.js
+++ js/scroll.js
@@ -1,34 +1,51 @@
+
+  const data = document.querySelectorAll('[data-scroll]');
+
+  if( navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+    data.forEach(function(element){
+      element.classList.add('animacaoScroll');
+
+    })
+  }
+  else {
 // Debounce do Lodash
 const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
+};
 
-const data = document.querySelectorAll('[data-scroll]');
 function scroll(){
-    const TopoTela = window.pageYOffset+((window.innerHeight*3)/5);
-    data.forEach(function(element){
-        if(TopoTela > element.offsetTop){
-            element.classList.add('animacaoScroll');
-        }
-    })
-      
+  const TopoTela = window.pageYOffset+((window.innerHeight*3)/5);
+  data.forEach(function(element){
+      if(TopoTela > element.offsetTop){
+          element.classList.add('animacaoScroll');
+      }
+  })
+    
 
 }
 
 if(data.length){
-    window.addEventListener('scroll',debounce(function(){
-    scroll();   
+  window.addEventListener('scroll',debounce(function(){
+  scroll();   
 },200));
 }
- 
+  }
+
